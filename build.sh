23fd29e167072a4c4313dc877fb34578543e5dac
Wolfram Hempel <hempel.wolfram@gmail.com>
2022-01-02 15:01:11 +0100

    updated build scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./node_modules/.bin/esbuild src/index-node.js --bundle --platform=node --outfile=dist/bundle-node.js
-./node_modules/.bin/esbuild src/index-browser.js --bundle --outfile=dist/bundle-browser.js
+./node_modules/.bin/esbuild src/index-browser.js --bundle --format=esm --outfile=dist/bundle-browser.js
