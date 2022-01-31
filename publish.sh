9032a56d1acbbc9f65122ce3ecb092a3956a2ac8
Wolfram Hempel <hempel.wolfram@gmail.com>
2022-01-31 12:07:04 +0100

    publishing version 1.1.1

--- publish.sh
+++ publish.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Make sure everything stops if command fails
-set -e 
-set -o pipefail
-
 # Show current version
 echo "Current version:" `npm view @hivekit/client-js version`
 
@@ -12,6 +8,11 @@ read -p "Enter the new version number: " version
 
 # Run tests?
 npm run test-only
+
+# Stop things if test failed
+if [ $? -ne 0 ]; then  
+    echo "TESTS FAILED"; exit 1; 
+fi
 
 # Replace version in package.json
 npm version ${version}
@@ -22,8 +23,8 @@ npm run build-browser-bundle
 # Commit and Push to git
 git add -A
 git commit -m "publishing version ${version}"
-git tag ${version}
 git push
+git tag ${version}
 git push --tags
 
 # Publish to npm
